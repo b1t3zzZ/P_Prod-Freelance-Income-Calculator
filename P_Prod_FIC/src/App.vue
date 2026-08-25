7bf69bb456476b4ac89095a90b9276141101567e
b1t3zzZ <tsibulevskiym@gmail.com>
2026-08-25 11:26:31 +0200

    sdsd

--- P_Prod_FIC/src/App.vue
+++ P_Prod_FIC/src/App.vue
@@ -3,9 +3,29 @@
 </script>
 
 <template>
-
+    <div class="buttons">
+        <button>Prix Total</button>
+        <button>CHF par heure</button>
+    </div>
+    <input type="text">
+    <input type="text" name="" id="">
 </template>
 
 <style scoped>
-
+div {
+    display: flex;
+    justify-content: center;
+    margin-bottom: 20px;
+}
+button {
+    background-color: #f0f0f0;
+    border: none;
+    padding: 10px 20px;
+    margin: 0px;
+    cursor: pointer;
+    font-size: 16px;
+}
+button:first-child {
+    background-color: rgb(55, 55, 255);
+}
 </style>
