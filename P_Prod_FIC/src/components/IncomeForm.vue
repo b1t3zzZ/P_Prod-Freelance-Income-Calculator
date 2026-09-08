775c1cfac80a11cc78c35d7c78b52a12c83bd1ab
b1t3zzZ <tsibulevskiym@gmail.com>
2026-09-08 11:23:56 +0200

    petite correction

--- P_Prod_FIC/src/components/IncomeForm.vue
+++ P_Prod_FIC/src/components/IncomeForm.vue
@@ -24,10 +24,22 @@ ul{
   display:flex;
   justify-content:space-around;
   gap:20px;
-
   list-style:none;
   margin:0;
   padding:0;
+}
+li{
+  cursor: pointer;
+  font-size:16px;
+  margin:10px 0;
+  padding:10px 20px;
+  border:1px solid black;
+  border-radius:15px;
+}
+li:hover{
+  color:red;
+  border:1px solid red;
+  box-shadow: 0px 0px 6px red;
 }
 
 </style>
